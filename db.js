f0d5b57e262745395e093d58e3f5354fb7e138d0
Tejashri <teju.phalke@gmail.com>
2019-12-02 18:03:22 +0530

    added

--- db.js
+++ db.js
@@ -3,7 +3,7 @@ const mysql=require("mysql")
 function connect()
 {
 const connection=mysql.createConnection({
-    host:'192.168.1.24',
+    host:'172.18.5.113',
     user:'root',
     password:'root',
     database:'myapp_db',
